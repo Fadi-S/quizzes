ec04de546942a57b7c0faeb28838f7a9ed7908bc
Fadi Sarwat <fadisarwat@yahoo.com>
2025-03-12 19:52:12 +0200

    Make numbers work if different language

--- app/Questions/CheckQuestion/Written.php
+++ app/Questions/CheckQuestion/Written.php
@@ -8,6 +8,39 @@ use App\Questions\QuestionResponse;
 
 readonly class Written implements CheckQuestion
 {
+    private function normalizeAnswer(string $answer): string
+    {
+        $answer = strtolower(trim($answer));
+
+        $numbersLetter = [
+            "one" => "1",
+            "two" => "2",
+            "three" => "3",
+            "four" => "4",
+            "five" => "5",
+            "six" => "6",
+            "seven" => "7",
+            "eight" => "8",
+            "nine" => "9",
+            "ten" => "10",
+        ];
+
+        $numbersArabic = [
+            "١" => "1",
+            "٢" => "2",
+            "٣" => "3",
+            "٤" => "4",
+            "٥" => "5",
+            "٦" => "6",
+            "٧" => "7",
+            "٨" => "8",
+            "٩" => "9",
+        ];
+
+        $answer = strtr($answer, $numbersLetter);
+        return strtr($answer, $numbersArabic);
+    }
+
     public function check(
         Question $question,
         array|int|string $answer,
@@ -16,9 +49,9 @@ readonly class Written implements CheckQuestion
 
         $options = $question->options
             ->pluck("name")
-            ->map(fn($name) => strtolower($name));
+            ->map(fn($name) => $this->normalizeAnswer($name));
 
-        $check = $options->contains(strtolower($answer));
+        $check = $options->contains($this->normalizeAnswer($answer));
 
         return new QuestionResponse(
             points: $check ? $question->points : 0,
